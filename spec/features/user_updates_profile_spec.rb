f2bb1f9bf7f345b55c33b3b87d089daed57670af
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-06-30 14:24:07 -0400

    update user test to verify updates persists

--- spec/features/user_updates_profile_spec.rb
+++ spec/features/user_updates_profile_spec.rb
@@ -9,6 +9,7 @@ I want to update my information
 So that I can keep my profile up to date" do
   scenario "authenticated user clinks link to update information" do
     user = FactoryGirl.create(:user)
+    email_update = 'newuseremail@email.com'
     visit '/'
     click_link("Sign In")
     fill_in 'Email', with: user.email
@@ -16,5 +17,11 @@ So that I can keep my profile up to date" do
     click_button('Log in')
     click_link 'Edit Profile'
     expect(page).to have_content("Edit User")
+    fill_in 'Email', with: email_update
+    fill_in 'Current password', with: user.password
+    click_button 'Update'
+    expect(page).to have_content("Your account has been updated successfully.")
+    click_link 'Edit Profile'
+    find_field('Email').value.should eq email_update
   end
 end
