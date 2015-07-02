5028bf26c7ebdc171e18159a3b8b22d9d9542923
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-02 15:41:31 -0400

    Hound edits

--- spec/features/user_creates_review_spec.rb
+++ spec/features/user_creates_review_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
-#Acceptance Criteria
-#[√] I want to be on the Bathrooms details page and see form to write a review
-#[√] I want to write a review for a specific bathroom
-#[√] I want to see it on the bathroom details page
+# Acceptance Criteria
+# [√] I want to be on the Bathrooms details page and see form to write a review
+# [√] I want to write a review for a specific bathroom
+# [√] I want to see it on the bathroom details page
 
 feature "As a user
 I want to write a review
@@ -17,19 +17,19 @@ So that I can inform others of the quality of the bathroom" do
       street_address: "62 Boylston",
       zip_code: "02116",
       description: "Back corner. Key from barista. Watch out for joe."
-      )
+    )
 
-      visit new_user_session_path
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      click_button "Log in"
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    click_button "Log in"
 
-      click_link "Check out bathrooms"
-      click_link @bathroom.location_name
-      fill_in "Body", with: "Very clean"
-      fill_in "Rating", with: 5
-      click_button "Submit"
-      expect(page).to have_content("Very clean")
-      expect(page).to have_content(5)
+    click_link "Check out bathrooms"
+    click_link @bathroom.location_name
+    fill_in "Body", with: "Very clean"
+    fill_in "Rating", with: 5
+    click_button "Submit"
+    expect(page).to have_content("Very clean")
+    expect(page).to have_content(5)
   end
 end
