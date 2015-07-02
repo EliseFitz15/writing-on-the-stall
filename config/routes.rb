2151abb4721629999555b50ad32ad1c955e08f0b
irnoble <irnoble59@gmail.com>
2015-07-02 15:53:06 -0400

    satisfied hound

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
 
   resources :bathrooms, only: [:index, :show, :new, :create, :destroy]
   resources :reviews, only: [:index, :show]
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
