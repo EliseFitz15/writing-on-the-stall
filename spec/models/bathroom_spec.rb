c09f0ce05119f9c7530dc2487ff2ab35336fa671
Kelsey Hale <kh.khale@gmail.com>
2015-07-06 12:38:42 -0400

    add model test for class method .search

--- spec/models/bathroom_spec.rb
+++ spec/models/bathroom_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Bathroom do
   it { should validate_presence_of(:street_address) }
   it { should validate_presence_of(:zip_code) }
   it { should validate_presence_of(:description) }
+
+  context 'when search method called on bathroom' do
+    describe '.search' do
+      it 'should search zip codes' do
+        expect(Bathroom.search("02116")).to eq Bathroom.where("zip_code = ?", "02116")
+      end
+    end
+  end
 end
