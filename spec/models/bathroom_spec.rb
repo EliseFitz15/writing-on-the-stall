402d1b5681972fdc5d0748c6de12bd2cb8375474
Kelsey Hale <kh.khale@gmail.com>
2015-07-06 14:09:45 -0400

    add unit test for search

--- spec/models/bathroom_spec.rb
+++ spec/models/bathroom_spec.rb
@@ -7,11 +7,18 @@ RSpec.describe Bathroom do
   it { should validate_presence_of(:zip_code) }
   it { should validate_presence_of(:description) }
 
-  context 'when search method called on bathroom' do
-    describe '.search' do
+  describe '.search' do
+    context 'there are results' do
       it 'should search zip codes' do
-        expect(Bathroom.search("02116")).to eq
-        Bathroom.where("zip_code = ?", "02116")
+        expect(Bathroom.search("02116")).to eq Bathroom.where(
+        "zip_code = ?",
+        "02116"
+        )
+      end
+    end
+    context "there are no matching results" do
+      it 'should search zip codes' do
+        expect(Bathroom.search("0211")).to eq []
       end
     end
   end
