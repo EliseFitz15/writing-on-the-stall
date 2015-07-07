9dfcdc9c272c1f39439c5af75daa5a205b2d47af
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-07 11:39:23 -0400

    add default profile photo

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -10,4 +10,8 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def default_url
+    "/assets/default_images/" + [version_name, "default.png"].compact.join('_')
+  end
 end
