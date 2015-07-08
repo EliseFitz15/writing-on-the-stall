c802c5b8f7b395c8edf0a6aadc721e5a47fe770d
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-08 17:49:25 -0400

    add unit test for avg_rating method

--- spec/models/bathroom_spec.rb
+++ spec/models/bathroom_spec.rb
@@ -23,4 +23,20 @@ RSpec.describe Bathroom do
       end
     end
   end
+  describe '.avg_rating' do
+    context 'there are reviews with ratings' do
+      it 'should calulate average rating for bathroom' do
+        bathroom = FactoryGirl.create(:bathroom)
+        review =  FactoryGirl.create(:review, bathroom: bathroom)
+        review2 = FactoryGirl.create(:review, rating: 3, bathroom: bathroom)
+        expect(bathroom.avg_rating).to eq(4.0)
+      end
+    end
+    context 'there are no reviews or ratings for this bathroom' do
+      it 'should return 0.0 for the average rating of the bathroom' do
+        bathroom = FactoryGirl.create(:bathroom)
+        expect(bathroom.avg_rating).to eq(0.0)
+      end
+    end
+  end
 end
