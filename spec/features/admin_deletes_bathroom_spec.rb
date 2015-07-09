9efebe4bad30113a3f2ac8a4859d5a8d2443cf6d
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-09 16:18:27 -0400

    fix hound

--- spec/features/admin_deletes_bathroom_spec.rb
+++ spec/features/admin_deletes_bathroom_spec.rb
@@ -7,9 +7,9 @@ require "rails_helper"
 feature "As an Admin
 I want to delete a bathroom
 so that the bathroom no longer exits on the site" do
-let(:admin) { FactoryGirl.create(:user, role: 'admin') }
-let!(:user) { FactoryGirl.create(:user) }
-let!(:bathroom) { FactoryGirl.create(:bathroom, user: user) }
+  let(:admin) { FactoryGirl.create(:user, role: 'admin') }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:bathroom) { FactoryGirl.create(:bathroom, user: user) }
   scenario "admin deletes bathroom" do
     visit new_user_session_path
     fill_in 'Email', with: admin.email
