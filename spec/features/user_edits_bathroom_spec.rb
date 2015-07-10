842fcb6e23fe278b41fbe5cbad8ee74cb34f2b67
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2015-07-10 15:21:06 -0400

    styling

--- spec/features/user_edits_bathroom_spec.rb
+++ spec/features/user_edits_bathroom_spec.rb
@@ -19,8 +19,8 @@ so that bathroom has relevant information" do
     click_button "Log in"
     visit bathroom_path(bathroom)
 
-    expect(page).to have_link("Edit this Bathroom")
-    click_link 'Edit this Bathroom'
+    expect(page).to have_button("Edit this Bathroom")
+    click_button 'Edit this Bathroom'
     fill_in :bathroom_location_name, with: "Ian is weird"
     fill_in :bathroom_street_address, with: "SYKE HES HOT"
     fill_in :bathroom_zip_code, with: "02243"
@@ -41,8 +41,8 @@ so that bathroom has relevant information" do
     click_button "Log in"
     visit bathroom_path(bathroom)
 
-    expect(page).to have_link("Edit this Bathroom")
-    click_link 'Edit this Bathroom'
+    expect(page).to have_button("Edit this Bathroom")
+    click_button 'Edit this Bathroom'
     fill_in :bathroom_street_address, with: "SYKE HES HOT"
     fill_in :bathroom_zip_code, with: "02243"
     fill_in :bathroom_description, with: "Brian's not too shabby either"
